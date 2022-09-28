d4a2b305a026c24f4b74917753cc085700fc2c73
Moringa School <Abdulbasid.sheikh@student.moringaschool>
2022-09-28 15:52:34 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+      
+    }
+    return newArray;
+  }
+  function countDown () {
+    let i=10;
+    while (i > -1) {
+        console.log(i--);
+        
+    }
+    return i;
+  }
+
+
+  
+
